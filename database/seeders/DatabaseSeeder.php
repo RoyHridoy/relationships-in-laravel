5ef9e8e88447e8d833853ea50b073339a9d48c86
RoyHridoy <royhridoy1428@gmail.com>
2024-10-14 19:34:05 +0600

    clean up

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ArticleSeeder::class,
-            EpisodeSeeder::class,
-            CourseSeeder::class
+
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,5 @@ use App\Models\Episode;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return Article::with('comments')->get();
-});
 
-Route::get('/insert', function () {
-    $article = Article::findOrFail(1);
-    $comment = new Comment();
-    $comment->commentable()->associate($article);
-    $comment->body = 'Hello form comments';
-    $comment->save();
 });
